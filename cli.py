3eb808a1a56c4f4a1309ae22db7062cdf9feb8fb
Robert Amour <robert@ferretech.io>
2020-04-11 17:11:37 -0700

    Don't concat if only one output file

--- cli.py
+++ cli.py
@@ -224,8 +224,16 @@ def main():
             filenames = [f for f in filenames if f.endswith('.csv')]
             stats_filenames = [f for f in stats_filenames if f.endswith('.csv')]
 
-            combined_csv = concat([read_csv(f) for f in filenames])
-            combined_stats_csv = concat([read_csv(f) for f in stats_filenames])
+            if len(filenames) > 1:
+                combined_csv = concat([read_csv(f) for f in filenames])
+            else:
+                combined_csv = read_csv(filenames[0])
+
+            if len(stats_filenames) > 1:
+                combined_stats_csv = concat([read_csv(f) for f in stats_filenames])
+            else:
+                combined_stats_csv = read_csv(stats_filenames[0])
+
             combined_csv = combined_csv.sort_values(by=['period', 'wavenumber'])
             combined_stats_csv = combined_stats_csv.sort_values(by=['period', 'wavenumber'])
             combined_stats_csv = combined_stats_csv.round(decimals=3)

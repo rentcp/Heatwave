fbc6f4dd2c42227dbb3f1e5933a33cadc04ca0e5
Robert Amour <robert@ferretech.io>
2020-04-14 18:40:26 -0700

    Add tests for angle math functions

---
+++ tests/test_angle_math.py
@@ -0,0 +1,54 @@
+from classes.aqua_positions import *
+import pytest
+
+
+longitude_math_test_cases = [
+    # a + b = c
+    [10, -10, 0],
+    [0, -10, -10],
+    [150, 30, 180],
+    [150, 40, -170],
+    [-30, 40, 10],
+    [-150, -30, -180],
+    [-150, -40, 170]
+]
+
+
+@pytest.mark.parametrize('test_case', longitude_math_test_cases)
+def test_normalize_longitude(test_case):
+    assert normalize_longitude_arithmetic(test_case[0] + test_case[1]) == test_case[2]
+
+
+intl_date_line_test_cases = [
+    # min_lon, max_lon, includes_prime_meridian, includes_intl_date_line
+    [0, 180, True, True],
+    [-170, 170, False, True],
+    [-179, 179, True, False],
+    [-10, 10, True, False],
+    [-170, -10, True, True],
+    [-180, -10, True, True],
+    [-180, -10, False, True]
+]
+
+
+@pytest.mark.parametrize('test_case', intl_date_line_test_cases)
+def test_includes_intl_date_line(test_case):
+    assert includes_intl_date_line(test_case[0], test_case[1], test_case[2]) == test_case[3]
+
+
+calc_longitude_angle_test_cases = [
+    # min_lon, max_lon, includes_prime_meridian, angle
+    [0, 10, True, 10],
+    [-10, 10, True, 20],
+    [-180, 180, True, 360],
+    [-180, 180, False, 0],
+    [-180, -10, True, 190],
+    [-180, -10, False, 170],
+    [-180, -0.001, True, 180.001],
+    [-180, -0.001, False, 179.999]
+]
+
+
+@pytest.mark.parametrize('test_case', calc_longitude_angle_test_cases)
+def test_calculate_longitude_angle(test_case):
+    assert calculate_longitude_angle_in_degrees(test_case[0], test_case[1], test_case[2]) == test_case[3]
